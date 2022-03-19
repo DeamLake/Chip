f991e495460e6321a2e764c3b0f80ad3ae5bef8b
deamlake <1115709562@qq.com>
2022-03-19 09:40:34 +0800

    add Logging micros
    For easy to use spdlog

--- Chips/src/Chips/EntryPoint.h
+++ Chips/src/Chips/EntryPoint.h
@@ -7,6 +7,9 @@ extern Chips::Application* Chips::CreateApplication();
 int main(int argc, char** argv)
 {
 	Chips::Log::Init();
+	CPS_CORE_WARN("Initialized Log!");
+	int a = 5;
+	CPS_CORE_INFO("Hello! Var = {0}",a);
 
 	auto app = Chips::CreateApplication();
 	app->Run();
--- Chips/src/Chips/Log.h
+++ Chips/src/Chips/Log.h
@@ -19,3 +19,16 @@ namespace Chips {
 
 }
 
+// Core log macros
+#define CPS_CORE_TRACE(...)	    ::Chips::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define CPS_CORE_INFO(...)		    ::Chips::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define CPS_CORE_WARN(...)	    ::Chips::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define CPS_CORE_ERROR(...)	    ::Chips::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define CPS_CORE_FATAL(...)	    ::Chips::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+													    
+// Client log macros				   	    
+#define CPS_TRACE(...)					::Chips::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define CPS_INFO(...)						::Chips::Log::GetClientLogger()->info(__VA_ARGS__)
+#define CPS_WARN(...)					::Chips::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define CPS_ERROR(...)					::Chips::Log::GetClientLogger()->error(__VA_ARGS__)
+#define CPS_FATAL(...)					::Chips::Log::GetClientLogger()->fatal(__VA_ARGS__)
